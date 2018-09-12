753e24b88a119fbccc7ddd4b04bcdb5096ffae9c
Dan Wilson <dan@thedatacollective.com.au>
2018-09-12 04:07:25 +0000

    update to mb_2016_reference data set

--- data-raw/mb-2016-reference.R
+++ data-raw/mb-2016-reference.R
@@ -7,7 +7,7 @@ path <- "/data/datasets/abs/asgs/"
 
 mb_files <- list.files(path, pattern = "MB", full.names = TRUE)
 
-mb_2016_reference <- lapply(mb_files, fread) %>%
+mb_2016_reference <- lapply(mb_files, fread, colClasses = "text") %>%
   rbindlist()
 
 devtools::use_data(mb_2016_reference, overwrite = TRUE, compress = "xz")
