63206bd16a16970ccc02aef7c28335792d2aee87
Dan Wilson <dan@thedatacollective.com.au>
2018-09-12 03:56:20 +0000

    Added Meshblock to statistical area reference table

---
+++ R/mb-2016-reference.R
@@ -0,0 +1,25 @@
+#' mb_2016_reference
+#'
+#' A dataset that creates a linkage between the granular Meshblock 2016 codes
+#' and the Australian Statistical Geography (ASGS) codes.
+#'
+#' @format A data frame / `data.table`  with 16 variables:
+#' \describe{
+#' \item{\code{MB_CODE_2016}}{Meshblock 2016 code}
+#' \item{\code{MB_CATEGORY_NAME_2016}}{Description about the meshblock geography}
+#' \item{\code{SA1_MAINCODE_2016}}{The full identifier for the Statistical Area 1 (SA1)}
+#' \item{\code{SA1_7DIGITCODE_2016}}{A 7 digit identifier for the Statistical Area 1 (SA1)}
+#' \item{\code{SA2_MAINCODE_2016}}{The full identifier for the Statistical Area 2 (SA2)}
+#' \item{\code{SA2_5DIGITCODE_2016}}{A 5 digit identifier for the Statistical Area 2 (SA2)}
+#' \item{\code{SA2_NAME_2016}}{Names for SA2 geographies}
+#' \item{\code{SA3_CODE_2016}}{A 5 digit identifier for the Statistical Area 3 (SA3)}
+#' \item{\code{SA3_NAME_2016}}{Names for SA3 geographies}
+#' \item{\code{SA4_CODE_2016}}{A 3 digit identifier for the Statistical Area 4 (SA4)}
+#' \item{\code{SA4_NAME_2016}}{Names for SA4 geographies}
+#' \item{\code{GCCSA_CODE_2016}}{An identifier for the Greater Capital City Statistical Area (GCCSA)}
+#' \item{\code{GCCSA_NAME_2016}}{Names for the Greater Capital City Statistical Area (GCCSA)}
+#' \item{\code{STATE_CODE_2016}}{Numeric code for the state}
+#' \item{\code{STATE_NAME_2016}}{Full state names (unabbreviated)}
+#' \item{\code{AREA_ALBERS_SQKM}}{Area of the meshblock in square kilometers}
+#' }
+"mb_2016_reference"
---
+++ data-raw/mb-2016-reference.R
@@ -0,0 +1,13 @@
+# create translation table for meshblock 2016 to SA1
+
+library(data.table)
+library(segmentr)
+
+path <- "/data/datasets/abs/asgs/"
+
+mb_files <- list.files(path, pattern = "MB", full.names = TRUE)
+
+mb_2016_reference <- lapply(mb_files, fread) %>%
+  rbindlist()
+
+devtools::use_data(mb_2016_reference, overwrite = TRUE, compress = "xz")
